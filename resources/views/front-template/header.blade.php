ea7e2915ef214c8cda1fa57df8e7ef9df3fae793
ledleledle <leontodsky@gmail.com>
2020-02-22 11:01:32 +0700

    gambar galery tidak burik

--- resources/views/front-template/header.blade.php
+++ resources/views/front-template/header.blade.php
@@ -4,7 +4,6 @@
                 <div class="col-lg-12">
                     <nav class="navbar navbar-expand-lg navbar-light">
                         <a class="navbar-brand" href="{{ url('') }}"> <img src="{{asset('front/img/HaruBiru.png') }}" alt="logo"> </a>
-                        
                         <button class="navbar-toggler" type="button" data-toggle="collapse"
                             data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
                             aria-expanded="false" aria-label="Toggle navigation">
