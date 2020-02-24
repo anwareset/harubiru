f3b86458f3552fc1657776602b9b6e6212dfa0b4
ledleledle <leontodsky@gmail.com>
2020-02-24 12:24:57 +0700

    form change profile done :) + update intervall of typewriter

--- public/front/js/typewriter.js
+++ public/front/js/typewriter.js
@@ -4,7 +4,7 @@ const cursorSpan = document.querySelector(".cursor");
 const textArray = ["Aku","Kamu","Semua"];
 const typingDelay = 200;
 const erasingDelay = 100;
-const newTextDelay = 1000; // Delay between current and next text
+const newTextDelay = 2500; // Delay between current and next text
 let textArrayIndex = 0;
 let charIndex = 0;
 
