e15d979e43458c3b5b07c716f30706cb74d878d0
anwareset <anwareset@gmail.com>
2020-02-28 19:15:29 +0700

    Prev-Next Posts in Article Details

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -22,9 +22,15 @@ class BlogController extends Controller
     {
     	$articles =  Posts::latest()->paginate(3);
         $widgets =  Posts::latest()->take(5)->get();
+        $categories = Categories::get();
+        $tags = Tags::get();
+
     	$contents = Posts::where('slug', $slug)->get();
-    	$categories = Categories::get();
-    	$tags = Tags::get();
-        return view('front.blog.article-details', compact('contents', 'widgets', 'articles', 'categories', 'tags'));
+        //$idPost = Posts::select('id')->where('slug', $slug)->get();
+    	
+        $previousPost = Posts::where('id', '<', $contents[0]->id)->orderBy('id','desc')->first();
+        $nextPost = Posts::where('id', '>', $contents[0]->id)->orderBy('id','asc')->first();
+
+        return view('front.blog.article-details', compact('contents', 'widgets', 'articles', 'categories', 'tags', 'nextPost', 'previousPost'));
     }
 }
