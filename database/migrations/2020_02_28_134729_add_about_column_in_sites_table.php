5e77512e2fea28d7e47eef2361f353557a8b5e5b
anwareset <anwareset@gmail.com>
2020-02-28 21:31:07 +0700

    SiteConfig Backend

---
+++ database/migrations/2020_02_28_134729_add_about_column_in_sites_table.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddAboutColumnInSitesTable extends Migration
+{
+
+    public function up()
+    {
+        Schema::table('sites', function (Blueprint $table) {
+            $table->string('about');
+        });
+    }
+
+    public function down()
+    {
+        Schema::table('sites', function (Blueprint $table) {
+            //
+        });
+    }
+}
