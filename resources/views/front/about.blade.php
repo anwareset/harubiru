b686eb6716337bdd083e858daacb9fc64d047fbd
anwareset <anwareset@gmail.com>
2020-03-02 19:53:00 +0700

    show about from site config

--- resources/views/front/about.blade.php
+++ resources/views/front/about.blade.php
@@ -5,17 +5,7 @@
 @section('content')
 <section class="sample-text-area">
     <div class="container box_1170">
-        <h3 class="text-heading">Cuplikan Singkat Dari Desa Jati</h3>
-        <p class="sample-text">
-            Desa Jati adalah desa yang mayoritas penduduknya petani, tempat pendidikannya SD, MI dan SMPN Tarokan serta. beberapa Ponpes yang berada di kecamatan Tarokan, Kabupaten Kediri, Jawa Timur, Indonesia. Di desa ini terdapat industri rumah tangga yaitu tahu tempe bengkel pembuat alat pertanian seperti mesin perontok padi/jagung yang bergerak dalam pembuatan tempe.
-        </p><br>
-        <h3 class="text-heading">Sejarah Desa Jati</h3>
-        <p class="sample-text">
-        	Sejarah Sejarah Sejarah Sejarah Sejarah Sejarah Sejarah Sejarah Sejarah Sejarah Sejarah Sejarah Sejarah Sejarah Sejarah Sejarah Sejarah Sejarah Sejarah Sejarah Sejarah Sejarah Sejarah Sejarah Sejarah Sejarah Sejarah Sejarah Sejarah Sejarah Sejarah Sejarah Sejarah Sejarah Sejarah Sejarah Sejarah Sejarah Sejarah Sejarah Sejarah Sejarah Sejarah Sejarah Sejarah Sejarah Sejarah Sejarah Sejarah Sejarah Sejarah Sejarah Sejarah Sejarah Sejarah Sejarah Sejarah Sejarah Sejarah Sejarah Sejarah Sejarah Sejarah Sejarah Sejarah Sejarah Sejarah Sejarah Sejarah Sejarah Sejarah Sejarah Sejarah Sejarah Sejarah Sejarah Sejarah Sejarah Sejarah Sejarah Sejarah Sejarah Sejarah Sejarah Sejarah Sejarah Sejarah Sejarah Sejarah Sejarah Sejarah Sejarah Sejarah Sejarah Sejarah.
-        </p><br>
-        <h3 class="text-heading">Wilayah Desa Jati</h3>
-        <iframe allowfullscreen="" frameborder="0" width="100%" height="450" src="https://www.google.com/maps/embed/v1/place?key=AIzaSyAVEa7caPZ0fywi9OYju7zf7TdQdnEgHMs&amp;q=Jati%2C+Tarokan&amp;zoom=15" style="width: 80%;height: 740px;margin-left: auto;margin-right: auto;display: block;"></iframe><br>
-        <h3 class="text-heading">Perangkat Desa</h3>
+        {!! App\Sites::first()->about !!}
     </div>
  
 </section>
