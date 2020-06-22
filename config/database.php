a0fe2ffaf9cd7807a33ca3b2ce14ac8d058a9a95
anwareset <anwareset@gmail.com>
2020-06-22 15:59:44 +0700

    belajar travisci

--- config/database.php
+++ config/database.php
@@ -85,6 +85,17 @@ return [
             'sslmode' => 'prefer',
         ],
 
+        'testing' => [
+            'driver' => mysql,
+            'database' => env('DB_DATABASE', 'homestead'),
+            'username' => env('DB_USERNAME', 'homestead'),
+            'password' => env('DB_PASSWORD', 'secret'),
+            'charset' => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix' => '',
+            'strict' => false,
+        ],
+
         'sqlsrv' => [
             'driver' => 'sqlsrv',
             'url' => env('DATABASE_URL'),
